a4e32d8d5b8602e0f96941fff6a1c972b23c548e
Andreas Runfalk <andreas@runfalk.se>
2017-03-11 05:12:03 +0100

    Marked 3.6 and PyPy as supported

--- setup.py
+++ setup.py
@@ -144,6 +144,8 @@ if __name__ == "__main__":
             "Programming Language :: Python :: 3.3",
             "Programming Language :: Python :: 3.4",
             "Programming Language :: Python :: 3.5",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: Implementation :: PyPy",
             "Topic :: Utilities"
         ),
         zip_safe=False,
